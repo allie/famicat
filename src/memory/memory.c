c53d7a6a24fae0bbb7964a22c8cdb42934978e35
Alison Saia <littlestcake@gmail.com>
2015-01-27 22:51:02 -0500

    First commit

---
+++ src/memory/memory.c
@@ -0,0 +1,17 @@
+#include "memory.h"
+
+uint8_t Memory_ReadByte(uint16_t addr) {
+	return 0;
+}
+
+uint16_t Memory_ReadWord(uint16_t addr) {
+	return 0;
+}
+
+void Memory_WriteByte(uint16_t addr, uint8_t val) {
+
+}
+
+void Memory_WriteWord(uint16_t addr, uint16_t val) {
+
+}
