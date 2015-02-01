ae3817963d8c325c84f0de4c02cf27d9b14b59ba
Jacob Mischka <x.mischka.x@gmail.com>
2015-01-31 23:15:20 -0600

    Removed dumb diffy thingy

--- diff.py
+++
@@ -1,9 +0,0 @@
-test = open("test.log")
-nestest = open("nestest.log")
-lineCount = 0
-
-for testLine, nestestLine in zip(test, nestest):
-    lineCount += 1
-    if testLine[:74] != nestestLine[:74]:
-        print(testLine[:74])
-        print(nestestLine[:74]+"\n")
