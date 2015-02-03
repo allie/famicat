40bbe58c923001464b648c1da6768ed7dede0dec
Jacob Mischka <x.mischka.x@gmail.com>
2015-02-03 16:23:14 -0600

    Started work on APU

--- src/apu/apu.h
+++ src/apu/apu.h
@@ -2,49 +2,83 @@
 #define APU_H
 
 #include "../common.h"
+#include "../memory/memory.h"
+
+#define HI_PASS_STRONG 225574
+#define HI_PASS_WEAK 57593
 
 typedef struct {
     WORD volume;
     WORD counter;
+    BYTE loop;
+    BYTE disable;
+    WORD period;
 } Envelope;
 
 typedef struct {
     Envelope envelope;
-    BYTE halt;
+    BYTE enabled;
+    BYTE length_halt;
     BYTE length;
-    WORD sample;
-    int timer;
-    int timer_period;
+    DWORD timer;
+    WORD timer_period;
     BYTE sweep_enable;
     BYTE sweep_period;
     BYTE negate;
     BYTE shift;
     BYTE duty;
+    WORD dac;
 } Square;
 
 typedef struct {
+    BYTE enabled;
+    BYTE control;
     BYTE halt;
-    WORD sample;
     BYTE length;
-    int timer;
-    int timer_period;
+    BYTE length_halt;
+    DWORD timer;
+    WORD timer_period;
+    BYTE linear_counter;
+    BYTE linear_halt;
+    BYTE linear_reload;
+    BYTE lookup_counter;
+    WORD dac;
 } Triangle;
 
 typedef struct {
     Envelope envelope;
+    BYTE period;
+    BYTE enabled;
+    BYTE length_halt;
+    BYTE mode;
+    BYTE shift;
     BYTE halt;
     BYTE length;
-    WORD sample;
-    int timer;
-    int timer_period;
+    DWORD timer;
+    WORD timer_period;
+    WORD dac;
 } Noise;
 
 typedef struct {
-    WORD sample;
-    BYTE irq;
-    BYTE bytes_remaining;
-    int timer;
-    int timer_period;
+    BYTE enabled;
+    BYTE irq_enable;
+    BYTE irq_throw;
+    BYTE loop_enable;
+    BYTE rate_index;
+    BYTE rate_counter;
+    BYTE direct_load;
+    BYTE direct_counter;
+    WORD data;
+    WORD sample_address;
+    WORD current_address;
+    BYTE sample_length;
+    BYTE sample_counter;
+    BYTE shift_counter;
+    WORD frequency;
+    BYTE has_sample;
+    DWORD timer;
+    WORD timer_period;
+    WORD dac;
 } DMC;
 
 typedef struct {
@@ -52,19 +86,34 @@ typedef struct {
     Triangle triangle;
     Noise noise;
     DMC dmc;
+    BYTE irq_enable;
+
+    BYTE frame_irq_throw;
+    BYTE frame_mode;
+    int frame_counter;
+    int frame_tick;
+    int last_frame_tick;
+
     double pulseOut[31];
     double tndOut[203];
+    double output;
+    int16_t sample;
 } APU;
 
 void APU_Init();
-double APU_MixOutput();
-void APU_Clock_Square(Square);
+void APU_Step();
+void APU_MixOutput();
+void APU_Clock_Square(Square, BYTE);
 void APU_Clock_Noise();
 void APU_Clock_Triangle();
 void APU_Clock_DMC();
-void APU_Read_Square(Square);
-void APU_Read_Triangle();
-void APU_Read_Noise();
-void APU_Read_DMC();
+void APU_Read();
+void APU_Write();
+void APU_Write_Square1();
+void APU_Write_Square2();
+void APU_Write_Triangle();
+void APU_Write_Noise();
+void APU_Write_DMC();
+void APU_Write_Common();
 
 #endif
