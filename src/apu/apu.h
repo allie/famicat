c2613073dadc425554ad29995a8b9855dc840e4f
Jacob Mischka <x.mischka.x@gmail.com>
2015-02-04 01:50:00 -0600

    Remade write and read functions because I understand why they work this way now. Still WIP

--- src/apu/apu.h
+++ src/apu/apu.h
@@ -3,6 +3,7 @@
 
 #include "../common.h"
 #include "../memory/memory.h"
+#include "../audio/core.h"
 
 #define HI_PASS_STRONG 225574
 #define HI_PASS_WEAK 57593
@@ -10,24 +11,30 @@
 typedef struct {
     WORD volume;
     WORD counter;
+    WORD divider_counter;
     BYTE loop;
     BYTE disable;
     WORD period;
+    BYTE reset;
 } Envelope;
 
 typedef struct {
     Envelope envelope;
     BYTE enabled;
-    BYTE length_halt;
+    BYTE halt;
     BYTE length;
-    DWORD timer;
-    WORD timer_period;
+    WORD timer;
+    WORD period;
     BYTE sweep_enable;
     BYTE sweep_period;
-    BYTE negate;
-    BYTE shift;
-    BYTE duty;
-    WORD dac;
+    BYTE sweep_counter;
+    BYTE sweep_reload;
+    BYTE sweep_negate;
+    BYTE sweep_shift;
+    BYTE duty_cycle;
+    BYTE duty_count;
+    BYTE num;
+    SWORD sample;
 } Square;
 
 typedef struct {
@@ -35,28 +42,28 @@ typedef struct {
     BYTE control;
     BYTE halt;
     BYTE length;
-    BYTE length_halt;
+    DWORD counter;
     DWORD timer;
+    DWORD timer_count;
     WORD timer_period;
-    BYTE linear_counter;
     BYTE linear_halt;
     BYTE linear_reload;
     BYTE lookup_counter;
-    WORD dac;
+    SWORD sample;
 } Triangle;
 
 typedef struct {
     Envelope envelope;
     BYTE period;
     BYTE enabled;
-    BYTE length_halt;
     BYTE mode;
     BYTE shift;
     BYTE halt;
     BYTE length;
     DWORD timer;
+    DWORD timer_count;
     WORD timer_period;
-    WORD dac;
+    SWORD sample;
 } Noise;
 
 typedef struct {
@@ -78,7 +85,7 @@ typedef struct {
     BYTE has_sample;
     DWORD timer;
     WORD timer_period;
-    WORD dac;
+    SWORD sample;
 } DMC;
 
 typedef struct {
@@ -94,26 +101,34 @@ typedef struct {
     int frame_tick;
     int last_frame_tick;
 
+    int64_t hipass_strong;
+    int64_t hipass_weak;
+
     double pulseOut[31];
     double tndOut[203];
     double output;
-    int16_t sample;
+    SWORD sample;
 } APU;
 
 void APU_Init();
 void APU_Step();
 void APU_MixOutput();
-void APU_Clock_Square(Square, BYTE);
-void APU_Clock_Noise();
-void APU_Clock_Triangle();
-void APU_Clock_DMC();
+void APU_ClockSquare(Square*);
+void APU_ClockNoise();
+void APU_ClockTriangle();
+void APU_ClockDMC();
 void APU_Read();
-void APU_Write();
-void APU_Write_Square1();
-void APU_Write_Square2();
-void APU_Write_Triangle();
-void APU_Write_Noise();
-void APU_Write_DMC();
-void APU_Write_Common();
+void APU_Write(WORD addr);
+void APU_WriteSquare(Square*);
+void APU_WriteTriangle();
+void APU_WriteNoise();
+void APU_WriteDMC();
+void APU_WriteCommon();
+void APU_HipassStrong();
+void APU_HipassWeak();
+void APU_Push();
+void APU_FrameSequencerStep();
+void APU_ClockSweep(Square*);
+void APU_ClockDecay(Envelope*);
 
 #endif
